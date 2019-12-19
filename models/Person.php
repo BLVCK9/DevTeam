45adfe8b4540cede8e54032dfa0334dc3749ba88
Natalia312 <marchot@list.ru>
2019-12-19 11:11:38 +0400

    My first commit

---
+++ models/Person.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "person".
+ *
+ * @property int $id
+ * @property string $lastname
+ * @property string $firstname
+ * @property string $middlename
+ */
+class Person extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'person';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['lastname', 'firstname', 'middlename'], 'required'],
+            [['lastname', 'firstname', 'middlename'], 'string', 'max' => 100],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'lastname' => 'Lastname',
+            'firstname' => 'Firstname',
+            'middlename' => 'Middlename',
+        ];
+    }
+}
