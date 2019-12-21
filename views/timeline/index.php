7d0da20fcf25136c41ce9b1445bbaeaebf41910f
BLVCK9 <nikitka501petrov@gmail.com>
2019-12-21 14:25:55 +0400

    Timeline

---
+++ views/timeline/index.php
@@ -0,0 +1,11 @@
+<?php
+	$this->title = 'Timeline';
+
+?>
+
+<div id="my-chart">
+	<h1><?= $this->title ?></h1>
+
+  <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=19teocd9qT_PNPyWhEer26Ez7lI8X8zq7uZot7DvT5N0&font=Default&lang=en&initial_zoom=2&height=650' width='100%' height='650' webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder='0'></iframe>
+
+</div>
